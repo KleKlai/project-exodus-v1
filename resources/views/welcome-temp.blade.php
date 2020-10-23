bdeca95691629727eed54202c2a45a8d3cdb8cd7
pnavarez <navarezphilip@gmail.com>
2020-10-24 03:21:53 +0800

    welcome temp page

---
+++ resources/views/welcome-temp.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.plain')
+
+@section('content')
+    <div class="mb-4">
+        <div id="pattern">
+            <img src="/images/spattern.png" alt="">
+        </div>
+
+        <div class="container flex space-x-10 my-4 justify-center">
+            <a href="#">Download Catalogs</a>
+            <a href="#">Schedule Appointment</a>
+            <a href="#">Watch Videos</a>
+        </div>
+
+        <img src="/images/covers/cover6.png" alt="cover" class="w-screen">
+
+        <div class="container mt-5 mb-4 px-24">
+            <div class="border-b border-minart-color-1 h-1"></div>
+        </div>
+
+        <div class="text-center font-bold text-2xl">
+            <h1 class="uppercase text-4xl">Catalogs</h1>
+            <div class="leading-7">
+                <div>Download Artwork Catalogs to view Artwork listings</div>
+                <div>and details in four different Galleries of Davao.</div>
+            </div>
+            <a href="#" class="btn btn-outline-dark mt-4 px-10" role="button" aria-disabled="true">Download Catalog</a>
+        </div>
+
+        <div class="mt-5">
+            <div class="text-center bg-minart-color-1 py-5 text-2xl font-bold">
+                <div class="text-4xl">Visit Mindanao Art 2020 Exhibit via Appointment.</div>
+                <a href="#" class="btn btn-outline-dark mt-4 px-10" role="button" aria-disabled="true">Schedule Appointment</a>
+            </div>
+        </div>
+
+        <div class="mt-5">
+            <div class="text-4xl text-center font-bold">
+                Mindanao Art 2020 speakers
+            </div>
+            <div class="container grid grid-flow-row grid-cols-2 grid-rows-2 mt-3">
+                {{-- <div class="mr-2 mb-2" style="height: 307px;">
+                    <video width="547" height="307">
+                        <source src="/videos/al_ryan.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div class="mb-2" style="height: 307px;">
+                    <video controls width="547" height="307">
+                        <source src="/videos/danny_rayos_del_sol.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div class="mr-2" style="height: 307px;">
+                    <video controls width="547" height="307">
+                        <source src="/videos/manalo.mp4" type="video/mp4">
+                    </video>
+                </div>
+                <div style="height: 307px;">
+                    <video controls width="547" height="307">
+                        <source src="/videos/baste_speech.mp4" type="video/mp4">
+                    </video>
+                </div> --}}
+            </div>
+        </div>
+
+        <div class="container p-4 flex">
+            Copyright 2020. Mindanao Art
+        </div>
+    </div>
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+        colors: {
+            'minart-color-1': '#c48935',
+        },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
