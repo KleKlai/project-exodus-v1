c4c0e38e557cf9e46a260af972c286399f76c0d2
pnavarez <navarezphilip@gmail.com>
2020-11-04 00:33:45 +0800

    artworks page pagination

--- resources/views/artworks.blade.php
+++ resources/views/artworks.blade.php
@@ -11,4 +11,16 @@
             @endforeach
 
         </div>
+
+        <div class="container flex flex-row items-center justify-center">
+            <a href="{{ $art->previousPageUrl() }}">
+                <i class="fas fa-chevron-left text-xl flex items-center pt-2"></i>
+            </a>
+            <div class="mx-2 px-3 pt-1 flex items-center bg-gray-200 rounded-full pb-1">
+                {{ $art->firstItem() }} to {{ $art->lastItem() }} of {{ $art->total() }}
+            </div>
+            <a href="{{ $art->nextPageUrl() }}">
+                <i class="fas fa-chevron-right text-xl pt-2"></i>
+            </a>
+        </div>
 @endsection
