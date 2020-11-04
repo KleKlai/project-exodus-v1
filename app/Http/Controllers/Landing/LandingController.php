1a52f0ed72b76d0be5fd9428df9349d6086a6cc1
pnavarez <navarezphilip@gmail.com>
2020-11-05 00:19:39 +0800

    artworks page pagination and filter

--- app/Http/Controllers/Landing/LandingController.php
+++ app/Http/Controllers/Landing/LandingController.php
@@ -20,12 +20,53 @@ class LandingController extends Controller
         ));
     }
 
-    public function artwork()
+    public function artwork(Request $request)
     {
-        // $art = Art::all();
-        $art = Art::paginate(8);
+        $museum = $request->get('museum', 0);
+        $gallery = urldecode($request->get('gallery', 'Gall Down South'));
 
-        return view('artworks', compact('art'));
+        // 0 - bakaw
+        // 1 - kulintang
+        // 2 - balangay
+        $galleries_arr = [
+            0 => [
+                'Gall Down South',
+                'Datu Bago',
+                'TheBauHaus',
+                'Sining Mata Visual Art & Music School',
+                'Deanna Sipaco (DS) Foundation for the Differently-Abled, Inc.',
+                'Kulit Kultura',
+            ],
+            1 => [
+                'Hinigalaay',
+                'Studio One Art Iligan',
+                'TINTA Artists Iligan',
+                'Irinugyun Artist',
+                'Talaandig Soil Painters',
+                'Erlow Talata',
+                'Ronald Tamfalan',
+            ],
+            2 => [
+                'Agusan Artist Association (AAA)',
+                'Alampat Gallery',
+                'Lumbayao Artist Collective',
+                'The Gallery of the Peninsula and the Archipelago',
+                'Good Times Café and Art Gallery (Zambo Norte)',
+            ],
+        ];
+
+        $galleries = $galleries_arr[$museum];
+
+        // $art = Art::all();
+        $users = User::where('gallery', $gallery)->pluck('id');
+        $art = Art::whereIn('user_id', $users)->paginate(8);
+
+        return view('artworks', compact(
+            'art',
+            'museum',
+            'gallery',
+            'galleries'
+        ));
     }
 
     public function artist()
--- resources/views/artworks.blade.php
+++ resources/views/artworks.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container flex flex-row">
+        <form action="/artworks" method="get" class="flex flex-row w-full">
+            <select name="museum" onchange="this.form.submit()" class="mr-3 w-1/4">
+                <option value="0" {{ ($museum == 0) ? 'selected' : '' }}>Bakaw</option>
+                <option value="1" {{ ($museum == 1) ? 'selected' : '' }}>Kulintang</option>
+                <option value="2" {{ ($museum == 2) ? 'selected' : '' }}>Balangay</option>
+            </select>
+
+            <select name="gallery" onchange="this.form.submit()" class="w-1/4">
+                @foreach ($galleries as $item)
+                    <option value="{{ $item }}" {{ ($item == $gallery) ? 'selected' : '' }}>{{ $item }}</option>
+                @endforeach
+            </select>
+        </form>
+    </div>
+
     <div id="columns">
-        @foreach($art as $artwork)
+        @foreach ($art as $artwork)
             <figure>
                 <a href="{{ route('art.show', $artwork) }}"><img src="{{ url('storage/artwork/'.$artwork->attachment) }}"></a>
             </figure>
@@ -10,16 +26,24 @@
     </div>
 
     <div class="container flex flex-row items-center justify-center">
-        <a href="{{ $art->previousPageUrl() }}">
-            <i class="fas fa-chevron-left text-xl flex items-center pt-1"></i>
-        </a>
+        @if ($art->previousPageUrl())
+            <a href="{{ $art->previousPageUrl() }}&museum={{ $museum }}&gallery={{ urlencode($gallery) }}">
+                <i class="fas fa-chevron-left text-xl flex items-center pt-1"></i>
+            </a>
+        @else
+            <i class="fas fa-chevron-left text-xl flex items-center text-gray-400"></i>
+        @endif
 
         <div class="mx-2 px-3 pt-1 flex items-center bg-gray-200 rounded-full pb-1">
             {{ $art->firstItem() }} to {{ $art->lastItem() }} of {{ $art->total() }}
         </div>
 
-        <a href="{{ $art->nextPageUrl() }}">
-            <i class="fas fa-chevron-right text-xl pt-1"></i>
-        </a>
+        @if ($art->nextPageUrl())
+            <a href="{{ $art->nextPageUrl() }}&museum={{ $museum }}&gallery={{ urlencode($gallery) }}">
+                <i class="fas fa-chevron-right text-xl pt-1"></i>
+            </a>
+        @else
+            <i class="fas fa-chevron-right text-xl text-gray-400"></i>
+        @endif
     </div>
 @endsection
